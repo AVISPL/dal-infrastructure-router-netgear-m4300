57ae733d917efa584e09d2aff83e37ea620d8c96
Maksym Rossiitsev <maksym.rossiitsev@avispl.com>
2020-02-05 19:34:01 +0200

    SYM-36: NetGear M4300 Switch integration test

--- src/test/java/com/avispl/symphony/dal/communicator/netgear/NetGearCommunicatorIntegrationTest.java
+++ src/test/java/com/avispl/symphony/dal/communicator/netgear/NetGearCommunicatorIntegrationTest.java
@@ -1,31 +1,20 @@
 package com.avispl.symphony.dal.communicator.netgear;
 
-import com.atlassian.ta.wiremockpactgenerator.WireMockPactGenerator;
 import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
+import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
 import com.avispl.symphony.api.dal.dto.monitor.Statistics;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.Assert;
-import org.junit.Rule;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-
-public class NetGearCommunicatorTest {
+@Tag("integration")
+public class NetGearCommunicatorIntegrationTest {
 
     static NetGearNASCommunicator netGearCommunicator;
-
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort().dynamicHttpsPort().bindAddress("127.0.0.1"));
-
-    {
-        wireMockRule.addMockServiceRequestListener(WireMockPactGenerator
-                .builder("netgear-adapter", "netgear")
-                .withRequestHeaderWhitelist("authorization", "content-type").build());
-        wireMockRule.start();
-    }
 
     @BeforeEach
     public void init() throws Exception {
@@ -38,33 +27,35 @@ public class NetGearCommunicatorTest {
     }
 
     @Test
-    public void netGearLoginOk() throws Exception {
+    public void getMultipleStatistics() throws Exception {
         List<Statistics> stat = netGearCommunicator.getMultipleStatistics();
-
         Assert.assertFalse(stat.isEmpty());
-        //netGearCommunicator.
     }
 
     @Test
-    public void controlPropertyReloadConfiguration() throws Exception {
+    public void controlPropertyStartupPort() throws Exception {
         ControllableProperty controllableProperty = new ControllableProperty();
-        controllableProperty.setProperty("Reload configuration");
+        controllableProperty.setProperty("Port 1/0/1");
+        controllableProperty.setValue("1");
         netGearCommunicator.controlProperty(controllableProperty);
+        Assert.assertEquals(((ExtendedStatistics)netGearCommunicator.getMultipleStatistics().get(0)).getStatistics().get("Port 1/0/1"), "true");
     }
-//
-//    @Test
-//    @Ignore
+
+    @Test
+    public void controlPropertyShutdownPort() throws Exception {
+        ControllableProperty controllableProperty = new ControllableProperty();
+        controllableProperty.setProperty("Port 1/0/1");
+        controllableProperty.setValue("0");
+        netGearCommunicator.controlProperty(controllableProperty);
+        Assert.assertEquals(((ExtendedStatistics)netGearCommunicator.getMultipleStatistics().get(0)).getStatistics().get("Port 1/0/1"), "false");
+    }
+
+    @Test
+    @Ignore
     public void controlPropertyReload() throws Exception {
         ControllableProperty controllableProperty = new ControllableProperty();
         controllableProperty.setProperty("Reload");
         netGearCommunicator.controlProperty(controllableProperty);
     }
 
-    @Test
-    public void controlPropertyRestartPort() throws Exception {
-        ControllableProperty controllableProperty = new ControllableProperty();
-        controllableProperty.setProperty("Port 1/0/1");
-        controllableProperty.setValue("1/0/1");
-        netGearCommunicator.controlProperty(controllableProperty);
-    }
 }
